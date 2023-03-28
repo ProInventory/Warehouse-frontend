6ee030529b152770a2a292b519e03d78b84bfc1b
Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
2023-03-29 01:55:51 +0530

    Designed basic UI components

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,24 +3,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 class Login extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <FontAwesomeIcon
-                    style={{ cursor: "pointer" }}
-                    onClick={this.props.onClick}
-                    icon={faUser}
-                    type="fa-solid"
-                    color="#000000"
-                />
-                {/* <h1>Login Form {faUser} </h1> */}
-                <input type="text" name="Name" />
-                <br />
-                <input type="text" name="Password" /> <br />
-                <button className="btn btn-primary">Submit</button>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div class="center">
+          <form method="post">
+            <div class="txt_field">
+              <div class="texts">
+                <label>Username</label>
+                <br></br>
+              </div>
+              <input type="text" name="Name" />
+              <br />
+            </div>
+            <div class="txt_field">
+              <div class="texts">
+                <label>Password</label>
+                <br></br>
+              </div>
+              <input type="text" name="Password" /> <br />
+            </div>
+            <button className="btn btn-primary">Log In</button>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Login;
