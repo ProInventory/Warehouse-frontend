e195f0173b8d4d1e2d38487c7c6479c451a55cab
Tharindu N <tharindu.nm@yahoo.com>
2023-04-03 18:20:06 +0530

    React-Toastify

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,32 +3,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 class Login extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div class="center">
-          <form method="post">
-            <div class="txt_field">
-              <div class="texts">
-                <label>Username</label>
-                <br></br>
-              </div>
-              <input type="text" name="Name" />
-              <br />
-            </div>
-            <div class="txt_field">
-              <div class="texts">
-                <label>Password</label>
-                <br></br>
-              </div>
-              <input type="password" name="Password" /> <br />
-            </div>
-            <button className="btn btn-primary">Log In</button>
-          </form>
-        </div>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <div class="center">
+                    <form>
+                        <div className="txt_field">
+                            <div class="texts">
+                                <label>Username</label>
+                                <br></br>
+                            </div>
+                            <input type="text" name="Name" />
+                            <br />
+                        </div>
+                        <div className="txt_field">
+                            <div className="texts">
+                                <label>Password</label>
+                                <br></br>
+                            </div>
+                            <input type="password" name="Password" /> <br />
+                        </div>
+                        <button className="btn btn-primary">Log In</button>
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Login;
