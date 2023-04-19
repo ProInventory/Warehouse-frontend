d2181b87ba8fe4a5555f19c8281df663ce4ff9c9
Tharindu N <tharindu.nm@yahoo.com>
2023-04-19 21:56:28 +0530

    Sidebar for all
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>
    Co-Authored-By: Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
    Co-Authored-By: Piumi De Alwis <128916734+piumi00@users.noreply.github.com>
    Co-Authored-By: Dulan Jayawickrama <115356046+realDulanJay@users.noreply.github.com>

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -1,13 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+import SideBar from "./sideBar";
+
+const Container = styled.div`
+	#parent {
+		display: table;
+		width: 100%;
+		height: 100%;
+	}
+
+	#sidebar {
+		display: table-cell;
+		width: 20%;
+		vertical-align: top;
+	}
+
+	#notFound {
+		background-color: #d1d1d1;
+		display: table-cell;
+		width: 80%;
+	}
+`;
 
 const NotFound = () => {
 	return (
 		<React.Fragment>
-			<div className="notFound">
-				<h1>404</h1>
-				<h2>Page not found</h2>
-				<p>Sorry, the page you are looking for does not exist.</p>
-			</div>
+			<Container>
+				<div id="#parent">
+					<div id="sidebar">
+						<SideBar />
+					</div>
+					<div id="notFound">
+						<div className="notFound">
+							<h1>404</h1>
+							<h2>Page not found</h2>
+							<p>
+								Sorry, the page you are looking for does not
+								exist.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Container>
 		</React.Fragment>
 	);
 };
--- src/components/status.jsx
+++ src/components/status.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
 import StatusPanel from "./status/status";
+import SideBar from "./sideBar";
+
+const Container = styled.div`
+	#parent {
+		display: table;
+		width: 100%;
+		height: 100%;
+	}
+
+	#sidebar {
+		display: table-cell;
+		width: 20%;
+		vertical-align: top;
+	}
+
+	#status {
+		background-color: #d1d1d1;
+		display: table-cell;
+		width: 80%;
+	}
+`;
 
 const Status = () => {
 	return (
 		<React.Fragment>
-			<StatusPanel />
+			<Container>
+				<div id="#parent">
+					<div id="sidebar">
+						<SideBar />
+					</div>
+					<div id="status">
+						<StatusPanel />
+					</div>
+				</div>
+			</Container>
 		</React.Fragment>
 	);
 };
