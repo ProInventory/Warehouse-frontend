52f6f567b358d46e06ef9a4179e43ff68ab8dff2
Tharindu N <tharindu.nm@yahoo.com>
2023-04-19 22:01:15 +0530

    404
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>
    Co-Authored-By: Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
    Co-Authored-By: Piumi De Alwis <128916734+piumi00@users.noreply.github.com>
    Co-Authored-By: Dulan Jayawickrama <115356046+realDulanJay@users.noreply.github.com>

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -21,6 +21,42 @@ const Container = styled.div`
 		display: table-cell;
 		width: 80%;
 	}
+
+	.not-found-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		background-color: #d1d1d1;
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+	}
+
+	.not-found-message {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.not-found-header {
+		font-size: 6rem;
+		font-weight: bold;
+		color: #3b5998;
+		margin-bottom: 0;
+	}
+
+	.not-found-text {
+		font-size: 2.5rem;
+		color: #606770;
+		margin-top: 0;
+		text-align: center;
+	}
+
+	.not-found-image {
+		margin-top: 2rem;
+		width: 10rem;
+	}
 `;
 
 const NotFound = () => {
@@ -33,12 +69,20 @@ const NotFound = () => {
 					</div>
 					<div id="notFound">
 						<div className="notFound">
-							<h1>404</h1>
-							<h2>Page not found</h2>
-							<p>
-								Sorry, the page you are looking for does not
-								exist.
-							</p>
+							<div className="not-found-container">
+								<div className="not-found-message">
+									<h1 className="not-found-header">Oops!</h1>
+									<p className="not-found-text">
+										We can't seem to find the page you're
+										looking for.
+									</p>
+								</div>
+								<img
+									src="/logo.png"
+									alt="ProInventory logo"
+									className="not-found-image"
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
