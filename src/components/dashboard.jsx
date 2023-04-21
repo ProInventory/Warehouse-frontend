9fdb1e7e356f6dc3d22be4ec2ac82253696737f7
Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
2023-04-21 19:01:15 +0530

    fixed minor bugs
    Co-Authored-By: Tharindu N. Madhusanka <mail@tharindu.dev>
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -8,56 +8,56 @@ import NumStat from "./dashboard/numStat";
 import Outlets from "./dashboard/outlets";
 
 const Container = styled.div`
-  display: flex;
-  height: 100vh;
-  flex-wrap: wrap; /* wrap children to new line if there isn't enough space */
+	display: flex;
+	height: 100vh;
+	flex-wrap: wrap; /* wrap children to new line if there isn't enough space */
 
-  @media (max-width: 1000px) {
-    & > div:first-child {
-      display: none; /* hide sidebar if screen is narrower than 1000px */
-    }
+	@media (max-width: 1000px) {
+		& > div:first-child {
+			display: none; /* hide sidebar if screen is narrower than 1000px */
+		}
 
-    & > div:last-child {
-      width: 100%; /* make the dash elements wrapper take up the entire width */
-    }
-  }
+		& > div:last-child {
+			width: 100%; /* make the dash elements wrapper take up the entire width */
+		}
+	}
 `;
 
 const SidebarWrapper = styled.div`
-  width: 20%;
+	width: 20%;
 `;
 
 const DashElementsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  background-color: #d1d1d1;
-  width: 80%;
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+	background-color: #d1d1d1;
+	width: 80%;
 `;
 
 const Dashboard = () => {
-  useEffect(() => {
-    document.title = "Dashboard | ProInventory";
-  }, []);
+	useEffect(() => {
+		document.title = "Dashboard | ProInventory";
+	}, []);
 
-  const [name, setName] = useState("User 1");
+	const [name, setName] = useState("User 1");
 
-  return (
-    <React.Fragment>
-      <Container>
-        <SidebarWrapper>
-          <SideBar user={name} />
-        </SidebarWrapper>
+	return (
+		<React.Fragment>
+			<Container>
+				<SidebarWrapper>
+					<SideBar user={name} />
+				</SidebarWrapper>
 
-        <DashElementsWrapper>
-          <NavBar />
-          <MainDash />
-          <NumStat />
-          <Outlets />
-        </DashElementsWrapper>
-      </Container>
-    </React.Fragment>
-  );
+				<DashElementsWrapper>
+					<NavBar />
+					<MainDash />
+					<NumStat />
+					<Outlets />
+				</DashElementsWrapper>
+			</Container>
+		</React.Fragment>
+	);
 };
 
 export default Dashboard;
--- src/components/dashboard/outlets.jsx
+++ src/components/dashboard/outlets.jsx
@@ -2,135 +2,149 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  .center {
-    width: clamp(100px, 50%, 200px);
-    height: 250px;
-    width: 200px;
-    background: #e8e8e8;
-    border-radius: 25px;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0);
-    transition: box-shadow 0.3s ease-in-out;
-    position: relative;
-  }
+	.center {
+		width: clamp(100px, 50%, 200px);
+		height: 250px;
+		width: 200px;
+		background: #e8e8e8;
+		border-radius: 25px;
+		box-shadow: 0px 0px 5px rgba(0, 0, 0, 0);
+		transition: box-shadow 0.3s ease-in-out;
+		position: relative;
+	}
 
-  .center:hover {
-    box-shadow: 0px 2px 10px rgba(128, 0, 128, 0.2);
-  }
+	.center:hover {
+		box-shadow: 0px 2px 10px rgba(128, 0, 128, 0.2);
+	}
 
-  .Title {
-    position: relative;
-    margin: 0px 0px;
-    padding: 20px 10px 25px 5px;
-    font-size: 24px;
-    font-weight: bold;
-    color: #6c757d;
-    font-family: "Rubik", sans-serif;
-  }
+	.Title {
+		position: relative;
+		margin: 0px 0px;
+		padding: 20px 10px 25px 5px;
+		font-size: 24px;
+		font-weight: bold;
+		color: #6c757d;
+		font-family: "Rubik", sans-serif;
+	}
 
-  .label-stat {
-    position: relative;
-    margin: 0px 0px;
-    padding: 15px 0px 0px 15px;
-    font-size: 24px;
-    font-weight: bold;
-    color: #6c757d;
-    font-family: "Rubik", sans-serif;
-  }
+	.label-stat {
+		position: relative;
+		margin: 0px 0px;
+		padding: 15px 0px 0px 15px;
+		font-size: 24px;
+		font-weight: bold;
+		color: #6c757d;
+		font-family: "Rubik", sans-serif;
+	}
 
-  .time-txt {
-    font-size: 16px;
-    font-weight: bold;
-    color: #6c757d;
-    font-family: "Rubik", sans-serif;
-    padding: 0px 116px 0px 15px;
-  }
+	.time-txt {
+		font-size: 16px;
+		font-weight: bold;
+		color: #6c757d;
+		font-family: "Rubik", sans-serif;
+		padding: 0px 116px 0px 15px;
+	}
 
-  .time-txt-sales {
-    font-size: 16px;
-    font-weight: bold;
-    color: #6c757d;
-    font-family: "Rubik", sans-serif;
-    padding: 40px 0px 0px 15px;
-  }
+	.time-txt-sales {
+		font-size: 16px;
+		font-weight: bold;
+		color: #6c757d;
+		font-family: "Rubik", sans-serif;
+		padding: 40px 0px 0px 15px;
+	}
 
-  .time-data {
-    font-size: 14px;
-    font-weight: regular;
-    color: #6c757d;
-    font-family: "Rubik", sans-serif;
-    padding: 0px 116px 0px 15px;
-  }
+	.time-data {
+		font-size: 14px;
+		font-weight: regular;
+		color: #6c757d;
+		font-family: "Rubik", sans-serif;
+		padding: 0px 116px 0px 15px;
+	}
 
-  .time-data-big {
-    font-size: 35px;
-    font-weight: 800;
-    color: #6c757d;
-    font-family: "Rubik";
-    padding: 0px 10px 0px 15px;
-    position: relative;
-    top: -10px;
-    transition: color 0.3s ease-in-out;
-  }
-  .time-data-big:hover {
-    color: #00000f;
-    transition: color 0.2s ease-in-out;
-  }
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-      monospace;
-  }
+	.time-data-big {
+		font-size: 35px;
+		font-weight: 800;
+		color: #6c757d;
+		font-family: "Rubik";
+		padding: 0px 10px 0px 15px;
+		position: relative;
+		top: -10px;
+		transition: color 0.3s ease-in-out;
+	}
+	.time-data-big:hover {
+		color: #00000f;
+		transition: color 0.2s ease-in-out;
+	}
+	code {
+		font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+			monospace;
+	}
 `;
 
 const NumStat = () => {
-  return (
-    <div style={{ position: "relative", top: -190, left: 45 }}>
-      <React.Fragment>
-        <div style={{ display: "flex" }}>
-          <Container style={{ marginRight: "20px" }}>
-            <label className="Title">My Outlets</label>
-            <div className="center">
-              <form>
-                <div className="txt_field">
-                  <label className="label-stat">WIP Store 01</label>
-                  <br></br>
-                </div>
-              </form>
-            </div>
-          </Container>
-          <Container style={{ marginRight: "20px", marginTop: "80px" }}>
-            <div className="center">
-              <form>
-                <div className="txt_field">
-                  <label className="label-stat">WIP Store 02</label>
-                  <br></br>
-                </div>
-              </form>
-            </div>
-          </Container>
-          <Container style={{ marginRight: "20px", marginTop: "80px" }}>
-            <div className="center">
-              <form>
-                <div className="txt_field">
-                  <label className="label-stat">WIP Store 03</label>
-                  <br></br>
-                </div>
-              </form>
-            </div>
-          </Container>
-          <Container style={{ marginRight: "20px", marginTop: "80px" }}>
-            <div className="center">
-              <form>
-                <div className="txt_field">
-                  <label className="label-stat">WIP Store 04</label>
-                  <br></br>
-                </div>
-              </form>
-            </div>
-          </Container>
-        </div>
-      </React.Fragment>
-    </div>
-  );
+	return (
+		<div style={{ position: "relative", top: -190, left: 45 }}>
+			<React.Fragment>
+				<div style={{ display: "flex" }}>
+					<Container style={{ marginRight: "20px" }}>
+						<label className="Title">My Outlets</label>
+						<div className="center">
+							<form>
+								<div className="txt_field">
+									<label className="label-stat">
+										WIP Store 01
+									</label>
+									<br></br>
+								</div>
+							</form>
+						</div>
+					</Container>
+					<Container
+						style={{ marginRight: "20px", marginTop: "80px" }}
+					>
+						<div className="center">
+							<form>
+								<div className="txt_field">
+									<label className="label-stat">
+										WIP Store 02
+									</label>
+									<br></br>
+								</div>
+							</form>
+						</div>
+					</Container>
+					<Container
+						style={{ marginRight: "20px", marginTop: "80px" }}
+					>
+						<div className="center">
+							<form>
+								<div className="txt_field">
+									<label className="label-stat">
+										WIP Store 03
+									</label>
+									<br></br>
+								</div>
+							</form>
+						</div>
+					</Container>
+					<Container
+						style={{ marginRight: "20px", marginTop: "80px" }}
+					>
+						<div className="center">
+							<form>
+								<div className="txt_field">
+									<label className="label-stat">
+										WIP Store 04
+									</label>
+									<br></br>
+								</div>
+							</form>
+						</div>
+					</Container>
+				</div>
+			</React.Fragment>
+		</div>
+	);
 };
 
 export default NumStat;
