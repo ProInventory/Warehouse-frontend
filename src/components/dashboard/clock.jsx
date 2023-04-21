4e243c99fc40c965cc9f3e1f83c91e0fb50c3859
Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
2023-04-21 22:48:35 +0530

    added new components
    Co-Authored-By: Tharindu N. Madhusanka <mail@tharindu.dev>
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>

--- src/components/dashboard/clock.jsx
+++ src/components/dashboard/clock.jsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 
 const Container = styled.div`
 	.center {
+		position: relative;
 		width: 400px;
 		height: 220px;
-		background: #e8e8e8;
 		border-radius: 25px;
-		box-shadow: 0px 0px 5px rgba(0, 0, 0, 0);
-		transition: box-shadow 0.3s ease-in-out;
-		position: relative;
+		overflow: hidden; /* this will hide any overflow of the pseudo-element */
+		background-image: url(night_gif.gif);
+		background-size: cover;
+		background-position: center;
 	}
 
 	.center:hover {
@@ -22,38 +23,22 @@ const Container = styled.div`
 		padding: 0px 0px 0px 15px;
 		font-size: 24px;
 		font-weight: bold;
-		color: #6c757d;
+		color: #ffffff;
 		font-family: "Rubik", sans-serif;
 	}
 
 	.time-txt {
 		font-size: 16px;
 		font-weight: bold;
-		color: #6c757d;
+		color: #d3d3d3;
 		font-family: "Rubik", sans-serif;
 		padding: 22px 0px 0px 15px;
-	}
-
-	.time-txt-sales {
-		font-size: 16px;
-		font-weight: bold;
-		color: #6c757d;
-		font-family: "Rubik", sans-serif;
-		padding: 40px 0px 0px 15px;
-	}
-
-	.time-data {
-		font-size: 14px;
-		font-weight: regular;
-		color: #6c757d;
-		font-family: "Rubik", sans-serif;
-		padding: 0px 116px 0px 15px;
 	}
 
 	.time-data-big {
 		font-size: 55px;
 		font-weight: 800;
-		color: #6c757d;
+		color: #ffffff;
 		font-family: "Rubik";
 		padding: 10px 10px 0px 15px;
 		position: relative;
@@ -61,7 +46,7 @@ const Container = styled.div`
 		transition: color 0.3s ease-in-out;
 	}
 	.time-data-big:hover {
-		color: #00000f;
+		color: #d3d3d3;
 		transition: color 0.2s ease-in-out;
 	}
 	code {
